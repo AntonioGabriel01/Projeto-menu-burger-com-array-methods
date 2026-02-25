451f01f40e705f9a76ec44412900d207687cc096
AntonioGabriel01 <edug51791@gmail.com>
2026-02-25 17:50:20 -0300

    Removendo espaços desnecessários

--- script.js
+++ script.js
@@ -14,94 +14,92 @@ const menuOptions = [
 
 const Botaofilter = document.querySelector(".filter");
 
+const BotaoSoma = document.querySelector(".Soma");
+const BotaoMap = document.querySelector(".map");
+const BotaoForeach = document.querySelector(".Foreach");
+const Lista = document.querySelector(".Lista");
+const formatador = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
 
-  const BotaoSoma = document.querySelector(".Soma");
-  const BotaoMap = document.querySelector(".map");
-  const BotaoForeach = document.querySelector(".Foreach");
-  const Lista = document.querySelector(".Lista");
-  const formatador = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-
-  /* Mostrar cardapio Foreach */
-  BotaoForeach.addEventListener("click", function () {
-    Lista.innerHTML = "";
-    menuOptions.forEach((item) => {
-      const li = document.createElement("li");
-
-      const img = document.createElement("img");
-      img.src = item.src;
-
-      const p = document.createElement("P");
-      p.textContent = item.name;
-
-      const price = document.createElement("p");
-      price.textContent = formatador.format(item.price);
-      price.classList.add("Price");
-
-      li.appendChild(img);
-      li.appendChild(p);
-      li.appendChild(price);
-      Lista.appendChild(li);
-    });
-  });
-  /* Mostrar cardapio Foreach */
-
-  /* Desconto com map */
-  BotaoMap.addEventListener("click", function () {
-    const Desconto = menuOptions.map((item) => {
-      return {
-        name: item.name,
-        price: item.price - item.price * 0.1,
-        vegan: item.vegan,
-        src: item.src,
-      };
-    });
-
-    Lista.innerHTML = "";
-    Desconto.forEach((item) => {
-      const li = document.createElement("li");
-
-      const img = document.createElement("img");
-      img.src = item.src;
-
-      const p = document.createElement("P");
-      p.textContent = item.name;
-
-      const price = document.createElement("p");
-      price.textContent = formatador.format(item.price);
-      price.classList.add("Price");
-
-      li.appendChild(img);
-      li.appendChild(p);
-      li.appendChild(price);
-      Lista.appendChild(li);
-    });
-  });
-  /* Desconto com map */
-
-  /*soma de todos os produtos*/
-  BotaoSoma.addEventListener("click", function () {
-    const soma = menuOptions.reduce((acc, item) => {
-      return acc + item.price;
-    }, 0);
-    Lista.innerHTML = "";
+/* Mostrar cardapio Foreach */
+BotaoForeach.addEventListener("click", function () {
+  Lista.innerHTML = "";
+  menuOptions.forEach((item) => {
     const li = document.createElement("li");
 
+    const img = document.createElement("img");
+    img.src = item.src;
+
     const p = document.createElement("P");
-    p.textContent =
-      "A soma de todos os itens do menu é igual a " + formatador.format(soma);
+    p.textContent = item.name;
 
-    Lista.appendChild(li);
+    const price = document.createElement("p");
+    price.textContent = formatador.format(item.price);
+    price.classList.add("Price");
+
+    li.appendChild(img);
     li.appendChild(p);
+    li.appendChild(price);
+    Lista.appendChild(li);
   });
-  /*soma de todos os produtos*/
+});
+/* Mostrar cardapio Foreach */
 
+/* Desconto com map */
+BotaoMap.addEventListener("click", function () {
+  const Desconto = menuOptions.map((item) => {
+    return {
+      name: item.name,
+      price: item.price - item.price * 0.1,
+      vegan: item.vegan,
+      src: item.src,
+    };
+  });
 
-  /* filtro */
-  Botaofilter.addEventListener("click", function () {
-    Lista.innerHTML = "";
+  Lista.innerHTML = "";
+  Desconto.forEach((item) => {
+    const li = document.createElement("li");
+
+    const img = document.createElement("img");
+    img.src = item.src;
+
+    const p = document.createElement("P");
+    p.textContent = item.name;
+
+    const price = document.createElement("p");
+    price.textContent = formatador.format(item.price);
+    price.classList.add("Price");
+
+    li.appendChild(img);
+    li.appendChild(p);
+    li.appendChild(price);
+    Lista.appendChild(li);
+  });
+});
+/* Desconto com map */
+
+/*soma de todos os produtos*/
+BotaoSoma.addEventListener("click", function () {
+  const soma = menuOptions.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
+  Lista.innerHTML = "";
+  const li = document.createElement("li");
+
+  const p = document.createElement("P");
+  p.textContent =
+    "A soma de todos os itens do menu é igual a " + formatador.format(soma);
+
+  Lista.appendChild(li);
+  li.appendChild(p);
+});
+/*soma de todos os produtos*/
+
+/* filtro */
+Botaofilter.addEventListener("click", function () {
+  Lista.innerHTML = "";
   const filtro = menuOptions.filter((value, index) => {
     if (value.vegan === true) {
       const li = document.createElement("li");
@@ -123,4 +121,4 @@ const Botaofilter = document.querySelector(".filter");
     }
   });
 });
- /* filtro */
+/* filtro */
